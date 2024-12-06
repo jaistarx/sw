7349dca875a52e83eb52afc06d4ab93867da5d8e
Jaison <57367854+jaistarx@users.noreply.github.com>
2024-12-06 16:41:59 +0530

    Update git-clone-script.cmd

--- git-clone-script.cmd
+++ git-clone-script.cmd
@@ -4,20 +4,18 @@
 setlocal enabledelayedexpansion
 
 :: Define the common parts of the repository URL
-set "baseUrl=git@github.com:Advantasure-RAQ/"
+set "baseUrl=https://github.com/user/"
 set "suffix=.git"
 
 :: Define repository names directly in the script (dynamic list)
-set repoNames[0]=member-services
-set repoNames[1]=mri
-set repoNames[2]=mrr-processor
-set repoNames[3]=mrr-services
-set repoNames[4]=radv-app
-set repoNames[5]=rightfax
-set repoNames[6]=tepalert-db
-set repoNames[7]=tepops-db
+set repoNames[0]=repo1
+set repoNames[1]=repo2
+set repoNames[2]=repo3
 :: Add or remove repositories as needed
-set /a repoCount=8
+set /a repoCount=3
+
+:: Default branch to switch to
+set "defaultBranch=dev"
 
 :: Calculate the last index of the repoNames array
 set /a repoCountFromZero=!repoCount!-1
@@ -31,7 +29,7 @@ echo.
 :: Show all repository names to be cloned with numbering (starting from 1)
 echo Repositories to be cloned:
 echo --------------------------------------------
-for /L %%i in (0,1,!repoCountFromZero!) do ( 
+for /L %%i in (0,1,!repoCountFromZero!) do (
     set "repoName=!repoNames[%%i]!"
     set /a repoNumber=%%i+1
     echo [!repoNumber!] !repoName!
@@ -45,13 +43,20 @@ if /I not "!userInput!"=="Y" (
     exit /B
 )
 
+:: Ask for the branch to switch to, default is dev
+set /p branchInput="Enter the branch to switch to after cloning (press Enter for default 'dev'): "
+if "!branchInput!"=="" (
+    set "branchInput=%defaultBranch%"
+)
+
+echo Branch to switch to: !branchInput!
+echo --------------------------------------------
+
 :: Loop through the array and construct full URLs, then clone each repository
-for /L %%i in (0,1,!repoCountFromZero!) do ( 
+for /L %%i in (0,1,!repoCountFromZero!) do (
     set "repoName=!repoNames[%%i]!"
     set "repoUrl=%baseUrl%!repoName!%suffix%"
-    set /a repoNumber=%%i+1
-    echo --------------------------------------------
-    echo [!repoNumber!] Cloning repository: !repoName!...
+    echo Cloning repository: !repoName!...
     echo --------------------------------------------
 
     git clone !repoUrl!
@@ -64,17 +69,17 @@ for /L %%i in (0,1,!repoCountFromZero!) do (
         echo [SUCCESS] Successfully cloned !repoUrl!
         :: Extract the directory name (repo name in this case)
         set "repoDir=!repoName!"
-        echo Switching to 'dev' branch in directory !repoDir!...
+        echo Switching to branch '!branchInput!' in directory !repoDir!...
         cd !repoDir!
         
-        :: Switch to 'dev' branch
-        git switch dev
+        :: Switch to the specified branch
+        git switch !branchInput!
         
         if errorlevel 1 (
-            echo [ERROR] Failed to switch to 'dev' branch in !repoDir! >> error.log
-            echo [ERROR] Failed to switch to 'dev' branch in !repoDir!
+            echo [ERROR] Failed to switch to branch '!branchInput!' in !repoDir! >> error.log
+            echo [ERROR] Failed to switch to branch '!branchInput!' in !repoDir!
         ) else (
-            echo [SUCCESS] Successfully switched to 'dev' branch in !repoDir!
+            echo [SUCCESS] Successfully switched to branch '!branchInput!' in !repoDir!
         )
         echo --------------------------------------------
         cd ..
